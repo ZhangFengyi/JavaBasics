58e8cfafbc1560268cdab790860973c2c2ec4326
zhangfy <someonenamedzfy@vip.163.com>
2020-07-18 00:26:53 +0800

    第一次添加文件

---
+++ src/com/zhangfy/java/basics/AriTest.java
@@ -0,0 +1,13 @@
+package com.zhangfy.java.basics;
+
+public class AriTest {
+
+	public static void main(String[] args) {
+		int i = 1;
+		i *= 0.1;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+	}
+
+}
