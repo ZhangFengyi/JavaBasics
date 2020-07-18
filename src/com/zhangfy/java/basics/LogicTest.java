73345969cfc7cc6fb58aa29b9c4396ccedb8ea6c
zhangfy <someonenamedzfy@vip.163.com>
2020-07-18 23:42:48 +0800

    添加&、&&、|、||练习

--- src/com/zhangfy/java/basics/LogicTest.java
+++ src/com/zhangfy/java/basics/LogicTest.java
@@ -1,8 +1,25 @@
 package com.zhangfy.java.basics;
 
 /*
- * Âß¼­ÔËËã²âÊÔ
+ * ï¿½ß¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
  */
 public class LogicTest {
-
+	
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;
+		if (x++ == 2 && ++y == 2) {
+			x = 7;
+		}
+		System.out.println("x = " + x + ",y = " + y);
+	
+		System.out.println("----------------------");
+		
+		int a = 1, b = 1;
+		if (a++ == 1 || ++b == 1) {
+			a = 7;
+		}
+		System.out.println("a = " + a + ", b = " + b);
+	}
+	
 }
